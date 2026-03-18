c1b0470176e2c17b2137a9edb18e24cf8f68a048
dugite <138967966+dugite-git@users.noreply.github.com>
2026-03-18 21:34:38 +0900

    Repositoryの実装

--- src/BudgetManagementBotSystem/Program.cs
+++ src/BudgetManagementBotSystem/Program.cs
@@ -1,6 +1,13 @@
 using BudgetManagementBotSystem.InfraStructure.Discord;
+using BudgetManagementBotSystem.InfraStructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("Db");
+
+builder.Services.AddDbContext<BudgetManagementDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddSingleton<DiscordBotService>();
 builder.Services.AddHostedService<Worker>();
